cb5c579cf510bccb843a3084eed8e032084a0ed5
scyr30 <suryasmpgembalabaik@gmail.com>
2025-08-07 07:27:14 +0700

    second commit

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&family=Inria+Serif&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Sour+Gummy:ital,wdth,wght@0,101.6,100..900;1,101.6,100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/app.css"/>
+    <link rel="stylesheet" href="/css/style.css"/>
 </head>
 <body>
  
